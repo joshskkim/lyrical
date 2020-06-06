1cd03d497a46f9f5776fc1e8d84df79884d975cb
joshskkim <josh.sk.kim@gmail.com>
2020-06-06 16:12:06 -0700

    Refactoring login process to occur on the backend

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,85 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import Player from './Player.jsx';
-import clientId from '../spotify.js';
-import "regenerator-runtime/runtime.js";
+// import React, { useState, useEffect } from 'react';
+// import "regenerator-runtime/runtime.js";
 
-export const authEndpoint = 'https://accounts.spotify.com/authorize';
+// const hash = window.location.hash
+//   .substring(1)
+//   .split('&')
+//   .reduce((init, item) => {
+//     if (item) {
+//       let parts = item.split('=');
+//       init[parts[0]] = decodeURIComponent(parts[1]);
+//     }
+//     return init;
+//   }, {});
 
-const redirectURI = 'http://localhost:9010';
-const scopes = ['user-read-currently-playing', 'user-read-playback-state'];
+// window.location.hash = '';
 
-const hash = window.location.hash
-  .substring(1)
-  .split('&')
-  .reduce((init, item) => {
-    if (item) {
-      let parts = item.split('=');
-      init[parts[0]] = decodeURIComponent(parts[1]);
-    }
-    return init;
-  }, {});
+// const App = () => {
+//   const [token, setToken] = useState('');
+//   const [item, setItem] = useState({});
+//   const [is_playing, setPlaying] = useState('Paused');
+//   const [progress_ms, setProgress] = useState(0);
 
-window.location.hash = '';
+//   const getNowPlaying = () => {
+//     spotifyAPI.getMyCurrentPlaybackState()
+//       .then((res) => {
+//         console.log(res);
+//       })
+//   }
 
-const App = () => {
-  const [token, setToken] = useState('');
-  const [item, setItem] = useState({});
-  const [is_playing, setPlaying] = useState('Paused');
-  const [progress_ms, setProgress] = useState(0);
+//   useEffect(() => {
+//     let loginToken = hash.access_token;
 
-  const getPlaying = async(token) => {
-    const response = await fetch('https://api.spotify.com/v1/me/player', {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    });
-    const data = await response.json();
-    console.log(data);
-    return data;
-  }
+//     if (loginToken) {
+//       setToken(loginToken);
+//       spotifyAPI.setAccessToken(token);
+//     }
+//   }, []);
 
-  const getPlayer = async() => {
-    try {
-      const res = await getPlaying(token);
-      setItem(res.item);
-      setPlaying(res.is_playing);
-      setProgress(res.progress_ms);
-    } catch (err) {
-      console.error(err);
-    }
-  }
+//   return (
+//     <div className="App">
+//       {!token && (
+//         <a
+//           className="btn btn--loginApp-link"
+//           href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectURI}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+//         >
+//           Login to Spotify
+//         </a>
+//       )}
+//       {token && (
+//         <div>
+//           YOU LOGGED IN DAWG
+//           {/* <Player
+//             item={item}
+//             is_playing={is_playing}
+//             progress_ms={progress_ms}
+//           /> */}
+//         </div>
+//       )}
+//     </div>
+//   )
+// }
 
-  useEffect(() => {
-    let loginToken = hash.access_token;
-
-    if (loginToken) {
-      setToken(loginToken);
-      getPlayer();
-    }
-  }, []);
-
-  return (
-    <div className="App">
-      {!token && (
-        <a
-          className="btn btn--loginApp-link"
-          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectURI}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
-        >
-          Login to Spotify
-        </a>
-      )}
-      {token && (
-        <div>
-          YOU LOGGED IN DAWG
-          {/* <Player
-            item={item}
-            is_playing={is_playing}
-            progress_ms={progress_ms}
-          /> */}
-        </div>
-      )}
-    </div>
-  )
-}
-
-export default App;
+// export default App;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,36 @@
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
+const querystring = require('querystring');
 
+// SPOTIFY AUTHENTICATION VARIABLES
+const { client_id, client_secret } = require('./spotify.js');
+const redirect_uri = 'http://localhost:9010';
+const stateKey = 'spotify_auth_state';
+
+// COOKIE GENERATOR
+const generateRandomString = (length) => {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let text = '';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+// MIDDLEWARE
 const app = express();
-const compression = require('compression');
-const bodyParser = require('body-parser');
 
-app.use(compression());
-app.use(express.static(path.join(__dirname, '../public/')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../public/')))
+   .use(cors())
+   .use(compression())
+   .use(bodyParser.urlencoded({ extended: true }))
+   .use(bodyParser.json())
+   .use(cookieParser());
 
 app.get('/api', (req, res) => {
 
