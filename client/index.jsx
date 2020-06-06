a4486859688ad31a60a1c70de9823f5c3b830ae6
joshskkim <josh.sk.kim@gmail.com>
2020-06-06 16:13:26 -0700

    Placeholder during refactor

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App.jsx';
+// import App from './components/App.jsx';
 
+const App = () => {
+  return (
+    <div>
+      Spotify player beep boop
+    </div>
+  )
+}
 
 render(<App />, document.getElementById('app'));
