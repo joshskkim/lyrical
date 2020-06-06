039571236ae741a954cb80c2bd57d7c6bdc17f98
joshskkim <josh.sk.kim@gmail.com>
2020-06-06 16:21:59 -0700

    Initial refactor for spotify login

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const querystring = require('querystring');
 
 // SPOTIFY AUTHENTICATION VARIABLES
 const { client_id, client_secret } = require('./spotify.js');
-const redirect_uri = 'http://localhost:9010';
+const redirect_uri = 'http://localhost:9010/callback';
 const stateKey = 'spotify_auth_state';
 
 // COOKIE GENERATOR
@@ -32,8 +32,102 @@ app.use(express.static(path.join(__dirname, '../public/')))
    .use(bodyParser.json())
    .use(cookieParser());
 
-app.get('/api', (req, res) => {
+// LOGIN ROUTE
+app.get('/login', (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
 
+  const scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
+
+// REFRESH AFTER LOGIN
+app.get('/callback', (req, res) => {
+  let code = req.query.code || null;
+  let state = req.query.state || null;
+  let storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    axios.post(authOptions, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+
+        let access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        let options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        axios.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+// REQUEST ACCESS FROM REFRESH TOKEN
+app.get('/refresh_token', (req, res) => {
+  const refresh_token = req.query.refresh_token;
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  axios.post(authOptions, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
 });
 
 module.exports = app;
