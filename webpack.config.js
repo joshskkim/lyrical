1e9d8c55a337c1b5eb45d4baaff3565e8ed360dd
joshskkim <josh.sk.kim@gmail.com>
2020-06-06 11:50:08 -0700

    Initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/app.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          // Inline files smaller than 10 kB (10240 bytes)
+          limit: 10 * 1024,
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        // This will apply the loader before the other ones
+        enforce: 'pre',
+      },
+    ]
+  },
+  // optimization: {
+  //   concatenateModules: true,
+  //   nodeEnv: 'production',
+  //   minimize: true,
+  // },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    fileName: 'lyrical.js',
+    publicPath: '/public/'
+  },
+}
