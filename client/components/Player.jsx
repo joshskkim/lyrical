feb6c613eb0b7e0823ba6526fd787b6915ca1209
joshskkim <josh.sk.kim@gmail.com>
2020-06-08 15:50:34 -0700

    Linting

--- client/components/Player.jsx
+++ client/components/Player.jsx
@@ -5,7 +5,7 @@ const Player = (props) => {
   const { item, is_playing, progress_ms, lyrics } = props;
 
   const backgroundStyles = {
-    backgroundImage:`url(${item.album.images[0].url})`,
+    backgroundImage: `url(${item.album.images[0].url})`,
   };
 
   const progressBarStyles = {
@@ -15,15 +15,15 @@ const Player = (props) => {
   const showTime = (ms) => {
     const min = Math.floor(ms / 60000);
     const sec = ((ms % 60000) / 1000).toFixed(0);
-    return `${min}:${sec < 10 ? '0': ''}${sec}`;
-  }
+    return `${min}:${sec < 10 ? '0' : ''}${sec}`;
+  };
 
   const toMS = (str) => {
     const msArr = str.split(':');
-    const min = parseInt(msArr[0]) * 60000;
-    const sec = parseInt(msArr[1]) * 1000;
+    const min = parseInt(msArr[0], 10) * 60000;
+    const sec = parseInt(msArr[1], 10) * 1000;
     return min + sec;
-  }
+  };
 
   const showLyrics = () => {
     let currentLyrics = '';
@@ -31,17 +31,17 @@ const Player = (props) => {
     for (i = 0; i < end; i += 1) {
       if (progress_ms <= 9000) {
         currentLyrics = '';
-      } else if (toMS(lyrics[i].substring(2, 6)) <= progress_ms){
+      } else if (toMS(lyrics[i].substring(2, 6)) <= progress_ms) {
         currentLyrics =`${lyrics[i].substring(10)}\n${lyrics[i + 1].substring(10)}\n${lyrics[i + 2].substring(10)}\n`;
       }
     }
     return currentLyrics;
-  }
+  };
 
   return (
     <div className={style.mainWrapper}>
       <div className={style.nowPlaying_img}>
-        <img src={item.album.images[1].url} />
+        <img src={item.album.images[1].url} alt="album art" />
       </div>
       <div className={style.nowPlaying_side}>
         <div className={style.nowPlaying_name}>
@@ -63,8 +63,9 @@ const Player = (props) => {
           />
         </div>
       </div>
-      <div className={style.background} style={backgroundStyles} />{" "}
-      <div className={style.lyrics} style={ {'whiteSpace':'pre-wrap'} }>
+      <div className={style.background} style={backgroundStyles} />
+      {' '}
+      <div className={style.lyrics} style={{ 'whiteSpace':'pre-wrap' }}>
         {showLyrics()}
       </div>
     </div>
